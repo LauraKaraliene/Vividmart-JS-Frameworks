c8d1131bd2a3db23f57eaff31e8be145629f831d
Laura Karaliene <Laura.Karaliene@stud.noroff.no>
2025-03-19 20:41:31 +0100

    adds checkout successfull page styling

--- src/pages/CheckoutSuccess.jsx
+++ src/pages/CheckoutSuccess.jsx
@@ -1,15 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
-// import "bootstrap/dist/css/bootstrap.min.css";
+import styles from "./CheckoutSuccess.module.scss";
 
 const CheckoutSuccess = () => {
   return (
     <div className="container text-center mt-5">
-      <div className="alert alert-success">
+      <div className="alert alert-success col-md-6 mx-auto shadow poppins">
         <h2>Thank you for your purchase!</h2>
         <p>Your order was successful.</p>
       </div>
-      <Link to="/" className="btn btn-primary">
+      <Link
+        to="/"
+        className={`btn btn-primary text-white py-2 px-4 mt-3 syncopate text-uppercase ${styles.backButton}`}
+      >
         Back to Store
       </Link>
     </div>
